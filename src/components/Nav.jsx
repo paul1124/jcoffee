0302fd92310b646bfd9c1275964616dc402c4203
Paul Hong <paulhong1124@gmail.com>
2019-11-25 14:26:57 -0800

    updates

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,15 +7,19 @@ const listener = e => {
     let currentScrollPos = window.pageYOffset;
     console.log(currentScrollPos);
     if(currentScrollPos < 150) {
-        document.getElementById("nav-logo").style.transform = "translateY(2em)";
+        document.getElementById("nav-logo").style.transform = "translateY(1em)";
         document.getElementById("nav-logo").style.fontSize = "8em";
         document.getElementById("nav-logo").style.backgroundColor = "rgba(0, 0, 0, 0.62)";
         document.getElementById("nav-logo").style.position = "absolute";
+
+        document.getElementById("nav-logo").style.display = "block";
     } else {
         document.getElementById("nav-logo").style.transform = "translateY(0em)";
         document.getElementById("nav-logo").style.background = "none";
         document.getElementById("nav-logo").style.fontSize = "2em";
-        document.getElementById("nav-logo").style.position = "relative";
+        // document.getElementById("nav-logo").style.position = "relative";
+
+        document.getElementById("nav-logo").style.display = "inline-block";
     }
     if(currentScrollPos < 170) {
         document.getElementById("nav").style.top = "0";
@@ -23,7 +27,7 @@ const listener = e => {
     else if(prevScrollPos > currentScrollPos) {
         document.getElementById("nav").style.top = "0";
     } else {
-        document.getElementById("nav").style.top = "-60px";
+        document.getElementById("nav").style.top = "-6em";
     }
     prevScrollPos = currentScrollPos;
 }
@@ -37,11 +41,11 @@ export default function Nav() {
     }, []);
     return (
         <nav className="nav" id="nav">
-            <ul id="navbar" className="navbar">
-                <li className="navbar-item"><a href="" className="nav-link">About</a></li>
-                <li id="nav-logo" className="navbar-item"><a href="" className="nav-link">-J-</a></li>
-                <li className="navbar-item"><a href="" className="nav-link">Contact</a></li>
-            </ul>
+            <div id="navbar" className="navbar">
+                <div className="navbar-item"><a href="" className="nav-link">About</a></div>
+                <div id="nav-logo" className="navbar-item"><a href="" className="nav-link">-J-</a></div>
+                <div className="navbar-item"><a href="" className="nav-link">Contact</a></div>
+            </div>
         </nav>
     )
 }
